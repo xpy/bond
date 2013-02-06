7c4b40bc2afc8c9e8bab6f98f30256b9b5037df6
xpy <pavloschris@gmail.com>
2013-02-06 15:37:39 +0200

    First Commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>EXP - Template</title>
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/main.css">
+
+    <script type="text/javascript" src="js/jquery-1.8.3.js"></script>
+    <script type="text/javascript" src="js/angular.js"></script>
+    <script type="text/javascript" src="js/ready.js"></script>
+
+    <script type="text/javascript" src="js/experiment.js"></script>  <!--Refactor That-->
+
+</head>
+<body>
+
+<div id="main-wrapper">
+<header><h1>Project Name</h1></header>
+	<section id="main-section"></section>
+	<footer></footer>
+</div>
+</body>
+</html>
